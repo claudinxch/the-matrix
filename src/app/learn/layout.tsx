66aab9918660dc22cfab6c0f4779f7104b42a010
claudinxch <claudiohenrique0909@gmail.com>
2024-12-04 20:35:47 -0300

    error fix

--- src/app/learn/layout.tsx
+++ src/app/learn/layout.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+import { Suspense } from 'react'
 import { Sidebar } from '@/components/sidebar'
 
 export default function LearnLayout({
@@ -7,7 +10,9 @@ export default function LearnLayout({
 }) {
   return (
     <div className="flex min-h-screen px-6 md:items-center md:justify-center">
-      <Sidebar className="w-64 px-4 py-8 hidden md:flex md:py-16 md:self-start" />
+      <Suspense fallback={<div>Loading sidebar...</div>}>
+        <Sidebar className="w-64 px-4 py-8 hidden md:flex md:py-16 md:self-start" />
+      </Suspense>
       <main className="flex min-h-screen flex-col gap-16 px-6 py-10 items-center md:items-start md:py-20">
         {children}
       </main>
